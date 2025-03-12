03441b8be70df06f8b01c5937dc3180e94bf0ff8
Chloe Osborne <chloe.osborne@uky.edu>
2025-03-12 15:09:12 -0400

    Initial commit

---
+++ Part_2_Sketch.ino
@@ -0,0 +1,19 @@
+void setup() {
+  // put your setup code here, to run once:
+// Hello sketch! I am sketching, woooo
+
+
+const int PWMB = 10; //speed control pin on the motor driver for the left motor 
+
+const int BIN2 = 9;  //control pin 2 on the motor driver for the left motor 
+
+const int BIN1 = 8;  //control pin 1 on the motor driver for the left motor 
+
+Serial.println(" This is me adding to Jaroo's example code!! ");   // Me being a scholar and changing something
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
