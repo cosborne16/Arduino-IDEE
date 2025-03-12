407769dd2166a99e4115bf9a95de64008ceb567e
Chloe Osborne <chloe.osborne@uky.edu>
2025-03-12 15:11:30 -0400

    Update Part_2_Sketch.ino

--- Part_2_Sketch.ino
+++ Part_2_Sketch.ino
@@ -3,13 +3,16 @@ void setup() {
 // Hello sketch! I am sketching, woooo
 
 
-const int PWMB = 10; //speed control pin on the motor driver for the left motor 
+ 
 
 const int BIN2 = 9;  //control pin 2 on the motor driver for the left motor 
 
 const int BIN1 = 8;  //control pin 1 on the motor driver for the left motor 
 
 Serial.println(" This is me adding to Jaroo's example code!! ");   // Me being a scholar and changing something
+
+
+Serial.println(" This is me double adding to Jaroo's example code!! ");   // Me being a scholar and changing something
 
 }
 
